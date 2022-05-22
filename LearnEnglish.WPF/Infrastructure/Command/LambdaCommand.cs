8166db7158608b116142b96f555a1bda00d5160b
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-05-22 18:35:28 +0300

    добавление базовой команды

---
+++ LearnEnglish.WPF/Infrastructure/Command/LambdaCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace LearnEnglish.WPF.Infrastructure.Command.Base
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Excecute;
+        private readonly Func<object, bool> _CanExecute;
+
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null!)
+        {
+            _Excecute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Excecute(parameter);
+    }
+}
