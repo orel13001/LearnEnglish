83d024d93415103526f9f05685ef57ef5cfb44a3
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-05-22 21:08:28 +0300

    Добавление базовой ViewModel

---
+++ LearnEnglish.WPF/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LearnEnglish.WPF.ViewModels.Base
+{
+    public abstract class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public virtual event PropertyChangedEventHandler PropertyChanged;
+
+        #region высвобождение управляемых ресурсов
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool Dispossig)
+        {
+            if (Dispossig || _Disposed) return;
+            _Disposed = true;
+            //высвобождение управляемых ресурсов
+        }
+        #endregion
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null!)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null!)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
