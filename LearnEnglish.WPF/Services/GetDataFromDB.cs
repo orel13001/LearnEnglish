a3b31be31d34637d1595d5333ae18e54575d256c
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-05-23 22:25:47 +0300

    Методы для получения номеров уроков и слов

---
+++ LearnEnglish.WPF/Services/GetDataFromDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using LearnEnglish.WPF.Models.EntityFramework;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnEnglish.WPF.Services
+{
+    public class GetDataFromDB
+    {
+        public ObservableCollection<int> GetWordNumberLesson()
+        {
+            ObservableCollection<int> numberLesson = new ObservableCollection<int>();
+            using (LearnEnglishContext db = new LearnEnglishContext())
+            {
+                numberLesson = new ObservableCollection<int>(db.DictionaryWords.Select(o => o.Lesson).Distinct().ToList());
+            }
+            return numberLesson;
+        }
+
+        public ObservableCollection<DictionaryWord> GetWordForNumberLesson(int numberLesson)
+        {
+            ObservableCollection<DictionaryWord> words = new ObservableCollection<DictionaryWord>();
+            using (LearnEnglishContext db = new LearnEnglishContext())
+            {
+                words = new ObservableCollection<DictionaryWord>(db.DictionaryWords.Where(o => o.Lesson == numberLesson));
+            }
+            return words;
+        }
+    }
+}
