0a7a3cd8d3a420d37b896609f287578e7f7b572a
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-05-24 19:53:16 +0300

    Создание объекта урока

---
+++ LearnEnglish.WPF/Models/Interfaces/ILesson.cs
@@ -0,0 +1,14 @@
+﻿using LearnEnglish.WPF.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnEnglish.WPF.Models.Interfaces
+{
+    public interface ILesson
+    {
+        public List<DictionaryWord> LessonItems { get; set; }
+    }
+}
