7d9b37863bac2eb245de4babc0f8daf4f8289813
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-05-24 22:06:14 +0300

    Рефакторинг

--- LearnEnglish.WPF/Services/GetDataFromDB.cs
+++ LearnEnglish.WPF/Services/GetDataFromDB.cs
@@ -11,27 +11,7 @@ namespace LearnEnglish.WPF.Services
 {
     public class GetDataFromDB
     {
-        public static ObservableCollection<int> GetWordNumberLesson()
-        {
-            ObservableCollection<int> numberLesson = new ObservableCollection<int>();
-            using (LearnEnglishContext db = new LearnEnglishContext())
-            {
-                numberLesson = new ObservableCollection<int>(db.DictionaryWords.Select(o => o.Lesson).Distinct().ToList());
-            }
-            return numberLesson;
-        }
-
-        public static ObservableCollection<DictionaryWord> GetWordForNumberLesson(int numberLesson)
-        {
-            ObservableCollection<DictionaryWord> words = new ObservableCollection<DictionaryWord>();
-            using (LearnEnglishContext db = new LearnEnglishContext())
-            {
-                words = new ObservableCollection<DictionaryWord>(db.DictionaryWords.Where(o => o.Lesson == numberLesson));
-            }
-            return words;
-        }
-
-
+      
         public static ObservableCollection<List<DictionaryWord>> GetLessonWord()
         {
             ObservableCollection<List<DictionaryWord>> lessonWord = new ObservableCollection<List<DictionaryWord>>();
