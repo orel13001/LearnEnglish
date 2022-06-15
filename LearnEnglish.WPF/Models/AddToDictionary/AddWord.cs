ed5a33cf37693ffa1e34cfb76e6778b3fc6ec72c
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-06-15 22:48:36 +0300

    Основа для добавления в словарь

---
+++ LearnEnglish.WPF/Models/AddToDictionary/AddWord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnEnglish.WPF.Models.AddToDictionary
+{
+    public class AddWord
+    {
+        public int Lesson { get; set; }
+        public string English { get; set; }
+        public string Transcription { get; set; }
+        public string Russian { get; set; }
+        public string? Comment { get; set; }
+        public string Pictures { get; set; }
+        public string Vois { get; set; }
+
+    }
+}
--- LearnEnglish.WPF/ViewModels/AddToDictionaryViewModel.cs
+++ LearnEnglish.WPF/ViewModels/AddToDictionaryViewModel.cs
@@ -1,13 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using LearnEnglish.WPF.Models.AddToDictionary;
+using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LearnEnglish.WPF.ViewModels
 {
-    public class AddToDictionaryViewModel
+    public class AddToDictionaryViewModel : Base.ViewModel
     {
+
+        private ObservableCollection<AddWord> _addWords;
+
+
+
+        public ObservableCollection<AddWord> AddWords 
+        {
+            get => _addWords;
+            //set => Set(ref _addWords, value);
+            set
+            {
+                _addWords = new ObservableCollection<AddWord>{ 
+                    new AddWord(){Lesson = 1},
+                    new AddWord(){Lesson = 1},
+                    new AddWord(){Lesson = 1},
+                    new AddWord(){Lesson = 1},
+                    new AddWord(){Lesson = 1},
+                    new AddWord(){Lesson = 1},
+                    new AddWord(){Lesson = 1}
+                };
+                
+            }
+        }
 
     }
 }
